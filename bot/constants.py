f3cc4a7649ead9b67c30a8995f6a7d1896584591
Serhoo <hlavatskyi.serhii@globaldev.tech>
2023-12-12 11:31:06 +0200

    Add pagination

--- bot/constants.py
+++ bot/constants.py
@@ -3,6 +3,12 @@ from enum import Enum
 
 class Actions(Enum):
     LIST_SESSIONS = 'list_sessions'
+    AUSBILDUNG_PAGE = 'ausbildung_page'
     LOAD_RECORDS = 'load_records'
     GET_SUMMARY = 'get_summary'
     BACK = 'back'
+
+
+SCRAPERS_TO_PAGES = {
+    'ausbildung': Actions.AUSBILDUNG_PAGE.value,
+}
